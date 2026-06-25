0459f704277b1554ce2371147406b6fa72e000f8
Patrick <sirpatrickcoleman@gmail.com>
2026-06-25 16:33:10 +0200

    More spell check

--- src/pages/projects/Clock.tsx
+++ src/pages/projects/Clock.tsx
@@ -10,19 +10,19 @@ type ClickableSquare = {
 }
 
 const CLICKABLE_SQUARES: ClickableSquare[] = [
-  { row: 2, col: 0, text: "After 30 seconds, this square will be filled for the first time. I hope you are still on this page to see it happen! Click some of next squares to see how long it will take for them." },
+  { row: 2, col: 0, text: "After 30 seconds, this square will be filled for the first time. I hope you are still on this page to see it happen! Click some of the next squares to see how long it will take for them." },
   { row: 3, col: 1, text: "I wouldn't try to hold your breath for the 6 minutes it will take to see this one change." },
-  { row: 4, col: 4, text: "In 1.5 hours, your body will have passively burnt approximately 110 calories(Assuming you are sitting at your computer). " },
+  { row: 4, col: 4, text: "In 1.5 hours, your body will have passively burnt approximately 110 calories (assuming you are sitting at your computer). " },
   { row: 6, col: 1, text: "Around 80g of cells in your body will have been replaced in 22 hours." },
   { row: 7, col: 3, text: "3,917,559 babies will have been born globally in the 11 days it takes to fill this square." },
-  { row: 8, col: 1, text: "If you left now and walked at ~5km/h, you could walk the entiretly of France's land border in this 32 days." },
+  { row: 8, col: 1, text: "If you left now and walked at ~5km/h, you could walk the entirety of France's land border in these 32 days." },
   { row: 10, col: 2, text: "If you planted a mango tree seed now, you would get ripe reddish fruit from the fully mature tree before this square ripened in just under 5 years." },
   { row: 12, col: 3, text: "You will not be alive to see this square turn red in 116 years. ;(" },
   { row: 13, col: 2, text: "In 1047 years, a snail that left now could have made it to the moon!" },
-  { row: 14, col: 2, text: "A Carbon-14 isotope will have lived out its 5700 year half life." },
+  { row: 14, col: 2, text: "A Carbon-14 isotope will have lived out its 5700 year half-life." },
   { row: 17, col: 1, text: "The Australian Tectonic Plate will have drifted almost 30km in the intervening 453 thousand years." },
-  { row: 20, col: 0, text: "Uranus' moon Desdemona will most likely have collided with another of it's inner moons, either Cressida or Juliet, in the next 98.1 million years."},
-  { row: 21, col: 0, text: "A Uranium-235 isotope will have lived out most of its 783 million year half life." },
+  { row: 20, col: 0, text: "Uranus' moon Desdemona will most likely have collided with another of its inner moons, either Cressida or Juliet, in the next 98.1 million years."},
+  { row: 21, col: 0, text: "A Uranium-235 isotope will have lived out most of its 783 million year half-life." },
   { row: 22, col: 1, text: "In 7 billion years the sun will have decayed into a red dwarf, signalling the final phase for our solar system, and I don't think our clock will be needed longer than that." },
 ]
 
